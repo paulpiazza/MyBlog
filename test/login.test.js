1dcef4d13c659d72aa5861859a7430fa9b17ab61
Paul Piazza <paul.piazza.pro@gmail.com>
2023-02-01 20:41:59 +0100

    install test env

---
+++ test/login.test.js
@@ -0,0 +1,8 @@
+const assert = require('chai').assert;
+
+describe('user login', () => {
+    it('should complete this test', (done) => {
+        assert.isBoolean("false", 'true is boolean')
+        done()
+    })
+});
