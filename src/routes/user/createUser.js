8f05e4715b0df48049eae14e3422533fc1129b17
Paul Piazza <paul.piazza.pro@gmail.com>
2023-02-04 19:23:50 +0100

    check inputs and tests /users/new route

--- src/routes/user/createUser.js
+++ src/routes/user/createUser.js
@@ -1,28 +1,55 @@
+const ExpressBrute = require('express-brute')
+const store = new ExpressBrute.MemoryStore();
+const User = require('../../models/User.js')
+const bcrypt = require('bcrypt')
+const bruteforce = new ExpressBrute(store)
+const { body, validationResult } = require('express-validator')
+const logger = require('../../../logs/logger')
+
 module.exports = (app) => {
 
-  app.post('/users/new', (req, res) => {
-    const User = require('../../models/User.js')
-    const bcrypt = require('bcrypt')
+  app.post(
 
-    bcrypt.hash(req.body.password, 10).then((hash) => {
-      const newUser = {
-        email: req.body.email,
-        password: hash
+    '/users/new',
+
+    bruteforce.prevent,
+
+    body('email')
+      .escape().trim().isEmail().normalizeEmail()
+      .withMessage('enter a valid email'),
+
+    body('password')
+      .trim().isLength({ min: 8, max: 12 })
+      .isStrongPassword()
+      .withMessage('between 8 and 12 characters.'),
+
+    (req, res) => {
+
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        const msg = errors.array().map(err => err.param).join(', ')
+        const errMsg = `${req.body.email} tries to create an account. Errors on ${msg}.`
+        logger.error(errMsg)
+        return res.status(400).json({ message: `${errMsg} Please check your credentials and send another request.` })
       }
 
-      const user = new User(newUser)
-      
-      user.save().then((userCreated) => {
-        const message = `New user created with email address: ${userCreated.email}. You can now login and get your token.`
-        res.json({ message, data: userCreated })
+      bcrypt.hash(req.body.password, 10).then((hash) => {
+        const newUser = {
+          email: req.body.email,
+          password: hash
+        }
+
+        User.create(newUser).then((userCreated) => {
+          const message = `New user created with email address: ${userCreated.email}. You can now login and get your token.`
+          res.json({ message, data: userCreated })
+        })
+
+      }).catch(err => {
+        const message = `Internal error. Please try later.`
+
+        res.status(500).json({ message, data: err })
+
       })
 
-    }).catch(err => {
-      const message = `Internal error. Please try later.`
-
-      res.status(500).json({ message, data: err })
-
     })
-
-  })
 }
---
+++ test/createUser.test.js
@@ -0,0 +1,124 @@
+const chai = require('chai')
+const assert = chai.assert
+const chaiHttp = require('chai-http')
+const app = require('../index')
+const bcrypt = require('bcrypt')
+const User = require('../src/models/User')
+chai.use(chaiHttp);
+const db = require('../src/mongodb/mongodb')
+const { triggerAsyncId } = require('async_hooks')
+const { resolve } = require('path')
+
+const saltRounds = 10
+const delay = 1000
+
+const tests = async () => {
+    return {
+        "new user": {
+            email: "admin@myblog.net",
+            password: process.env.ADMIN_PWD_TEST
+        },
+
+        "login fails": [
+            {
+                email: "admin@myblog",
+                password: process.env.ADMIN_PWD_TEST
+            },
+            {
+                email: "adminmyblog.net",
+                password: process.env.ADMIN_PWD_TEST
+            }
+        ],
+
+        "password fails": [
+            {
+                email: "admin1@myblog.net",
+                password: "123"
+            },
+            {
+                email: "admin2@myblog.net",
+                password: "Adm1465654482*"
+            },
+            {
+                email: "admin3@myblog.net",
+                password: "AdmH45678"
+            }
+        ]
+    }
+
+}
+
+describe('when the client try to sign in /users/new', function () {
+
+    this.timeout(2500)
+
+    it('should get status OK', async function () {
+
+        const testCredentials = await tests()
+
+        const res = await chai
+            .request(app)
+            .post('/users/new')
+            .send(testCredentials["new user"])
+
+        assert.equal(res.status, 200)
+    })
+
+    it('should get error 400 with bad login', async function () {
+
+        const testCredentials = await tests()
+
+        testCredentials["login fails"].forEach(async (credentials) => {
+
+            const res = await chai
+                .request(app)
+                .post('/users/new')
+                .send(credentials)
+
+            assert.equal(res.status, 400)
+        })
+    })
+
+    it('should get error 400 with bad password', async function () {
+
+        const testCredentials = await tests()
+
+        testCredentials["password fails"].forEach(async (credentials) => {
+
+            const res = await chai
+                .request(app)
+                .post('/users/new')
+                .send(credentials)
+
+            assert.equal(res.status, 400)
+        })
+    })
+
+    it('should get error 429 - too many requests', async function () {
+
+        const bruteForceLimit = 3
+
+        for (let i = 1; i <= bruteForceLimit; ++i) {
+            const res = await chai
+                .request(app)
+                .post('/users/new')
+                .send({
+                    email: "adminbruteforce@myblog.net",
+                    password: "123"
+                })
+        }
+        
+        assert.equal(res.status, 429, 'Brute force Not Working')
+    })
+
+    after(async function () {
+
+        const count = await User.find().count()
+
+        if (count > 0) {
+            await User.deleteMany()
+            await User.syncIndexes()
+        }
+    })
+
+});
